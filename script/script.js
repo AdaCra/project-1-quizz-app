35ecadb5a8a396369272180ced5e8a607905f61b
Adam Hannath <ahannath@gmail.com>
2023-03-27 23:11:31 +0200

    form page complete

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// Bookmarks
 function toggleBookmark(obj) {
   const bookmarkClear = "./assets/bookmark.png";
   const bookmarkFilled = "./assets/bookmark_filled.png";
@@ -10,23 +11,19 @@ function toggleBookmark(obj) {
   return;
 }
 
-// hide and reveal answer
+// Hide and Reveal Answer
 
 const revealAnswerButton = document.querySelectorAll(
   '[data-js="card_answer_button"]'
 );
+
 console.log(revealAnswerButton);
 revealAnswerButton.forEach((Button) => {
+  //
   Button.addEventListener("click", (event) => {
     console.log(event.target.children);
     event.target.firstElementChild.classList.toggle("hidden");
     event.target.lastElementChild.classList.toggle("hidden");
-    // if (event.target.classlist.includes("hidden")) {
-    //   event.target.classlist.remove("hidden");
-    // } else {
-    //   event.target.classlist.add("hidden");
-    // }
   });
+  //
 });
-
-function revealAnswer(obj) {}
